2cb47230b9fb1245c8dd88b05d6c24ae563b3d32
mcericson <mark.ericson@woodbury.edu>
2021-02-09 22:14:20 -0800

    Updates to class files

---
+++ 210210/Sphere_01_Rhino/Spherical Curve_01.py
@@ -0,0 +1,46 @@
+# Mark Ericson 2.08.21
+#This program creates single curve from a set of moving spherical coordinates
+
+#This portion of the program imports our custom modules.  They must be reloaded everytime a change is made.
+import TrigCircle
+from TrigCircle import TrigCirc
+from imp import reload
+reload(TrigCircle)
+
+#This imports rhinoscript and allows access to all of rhinos tools.
+import rhinoscriptsyntax as rs
+import math
+import random
+
+
+delSet = rs.AllObjects(True)
+rs.DeleteObjects(delSet)
+
+
+
+#create an empty list to store spheres and points respectively 
+Spheres = [] 
+Points =  []
+
+#create a for loop to generate i number of points about a sphere and then do things with those points.
+for i in range(1,180,1):
+    r = random.randint(20,60)
+    pts = TrigCirc(10,.5,100,i,0,0,0,"ThreeD")
+    sphere = rs.AddSphere(pts,r)
+    Spheres.append(sphere)
+    pt = rs.AddPoint(pts[0],pts[1],pts[2])
+    Points.append(pt)
+
+#union the spheres together
+Mass = rs.BooleanUnion(Spheres)
+for i in Mass:
+    Object = rs.AddMaterialToObject(i)
+    rs.MaterialColor(Object,(12,59,101))
+rs.AddPolyline(Points)
+
+
+
+rs.ZoomExtents()
+rs.CreatePreviewImage("Sphere.png")
+
+
